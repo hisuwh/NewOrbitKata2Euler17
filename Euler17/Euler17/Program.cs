564272af5d2cba886dada5f12aeb2e84d0610e87
Henry Ing-Simmons <hing-simmons@neworbit.co.uk>
2015-03-26 14:01:20 +0000

    Psuedocode

--- Euler17/Euler17/Program.cs
+++ Euler17/Euler17/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+
+            // X' - where X' = 1-19     = X'.length
+            // YX - where Y > 1         = Y.length + X.length 
+            // YX' - where Y' = 1-19    = Y.length + 'hundredand'.length + X'.length
+            // ZYX - where Y > 1        = Z.length + 'hundredand'.length + Y.length + X.length
+            // T000                     = T.length + 'thousand'.length
+
+            // X' = 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen' , 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+            // Y = 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
         }
     }
 }
